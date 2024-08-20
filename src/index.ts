204027b6a193924c8c7dc8fcce77aec830428c6e
Angel Popov <geleto@gmail.com>
2024-08-20 15:51:51 +0100

    chore: Add AddEmptyLineExtension to support adding empty lines in templates

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('Hello, world!');
+import * as nunjucks from 'nunjucks';
+import { Parser } from 'nunjucks/src/parser';
+import { Literal, Output } from 'nunjucks/src/nodes';
+
+export class AddEmptyLineExtension implements nunjucks.Extension {
+	tags = ['addEmptyLine'];
+
+	parse(parser: Parser) {//}, lexer: Lexer) {
+		// Get the token for the start of the tag
+		const tok = parser.nextToken();
+
+		if (!tok) {
+			throw new Error('Unexpected end of template input');
+		}
+
+		// Create a new literal node containing a newline character
+		const newlineNode = new Literal(tok.lineno, tok.colno, '\n');
+
+		// Advance past the 'addEmptyLine' tag
+		parser.advanceAfterBlockEnd(tok.value);
+
+		// Return the newline node as the output of the extension
+		return new Output(tok.lineno, tok.colno, [newlineNode]);
+	}
+}
+
+// Example usage with a template string
+const env = new nunjucks.Environment();
+env.addExtension('AddEmptyLineExtension', new AddEmptyLineExtension());
+const template = env.renderString('{% addEmptyLine %}Hello, world!', {});
+console.log(template);
