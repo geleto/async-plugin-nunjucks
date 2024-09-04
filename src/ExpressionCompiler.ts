9d5c1dfd5791340e524bd3897073225e5c1e82f7
Angel Popov <geleto@gmail.com>
2024-09-04 12:50:33 +0100

    feat: Add functionality in ExpressionCompiler to wait for all expressions to finish

--- src/ExpressionCompiler.ts
+++ src/ExpressionCompiler.ts
@@ -3,7 +3,12 @@ import { compiler, runtime, nodes } from 'nunjucks';
 
 type AsyncCompiledExpression = (env: nunjucks.Environment, context: object, frame: runtime.Frame, rnt: typeof runtime) => Promise<any>;
 
+/**
+ * Use a differnet compiler for each template
+ */
 export class ExpressionCompiler extends compiler.Compiler {
+	private runningExpressions: Promise<any>[] = [];
+
 	constructor(name: string, throwOnUndefined: boolean) {
 		super(name, throwOnUndefined);
 	}
@@ -19,13 +24,20 @@ export class ExpressionCompiler extends compiler.Compiler {
 		const code = this.getCode();
 		return new Function('context', 'env', 'runtime',
 			`return (async () => {
-                ${code}
-                return expressionResult;
-            })();`
+        ${code}
+        return expressionResult;
+      })();`
 		) as AsyncCompiledExpression;
 	}
 
 	evaluate(compiledFunc: AsyncCompiledExpression, env: nunjucks.Environment, context: object, frame: runtime.Frame, rnt: typeof runtime) {
-		return compiledFunc(env, context, frame, rnt);
+		const expressionPromise = compiledFunc(env, context, frame, rnt);
+		this.runningExpressions.push(expressionPromise);
+		return expressionPromise;
+	}
+
+	async waitAllExpressions(): Promise<void> {
+		await Promise.all(this.runningExpressions);
+		this.runningExpressions = []; // Clear the array after all expressions are resolved
 	}
 }
