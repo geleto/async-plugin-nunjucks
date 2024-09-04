a28cf87d9e46e80debd910bc44237164cc0975e0
Angel Popov <geleto@gmail.com>
2024-09-04 14:55:01 +0100

    feat: Expression compiler result function returns a Promise, implements a function to wait all expressions to finish

--- src/ExpressionCompiler.ts
+++ src/ExpressionCompiler.ts
@@ -1,9 +1,14 @@
 import * as nunjucks from 'nunjucks';
 import { compiler, runtime, nodes } from 'nunjucks';
 
-type CompiledExpression = (env: nunjucks.Environment, context: object, frame: runtime.Frame, rnt: typeof runtime) => any;
+type AsyncCompiledExpression = (env: nunjucks.Environment, context: object, frame: runtime.Frame, rnt: typeof runtime) => Promise<any>;
 
-export class ExpressionCompiler extends compiler.Compiler {
+/**
+ * Use a differnet compiler for each template
+ */
+export class AsyncExpressionCompiler extends compiler.Compiler {
+	private runningExpressions: Promise<any>[] = [];
+
 	constructor(name: string, throwOnUndefined: boolean) {
 		super(name, throwOnUndefined);
 	}
@@ -17,10 +22,22 @@ export class ExpressionCompiler extends compiler.Compiler {
 		this._emit(';');
 
 		const code = this.getCode();
-		return new Function('context', 'env', 'runtime', code + '; return expressionResult;');
+		return new Function('context', 'env', 'runtime',
+			`return (async () => {
+        ${code}
+        return expressionResult;
+      })();`
+		) as AsyncCompiledExpression;
 	}
 
-	evaluate(compiledFunc: CompiledExpression, env: nunjucks.Environment, context: object, frame: runtime.Frame, rnt: typeof runtime) {
-		return compiledFunc(env, context, frame, rnt);
+	evaluate(compiledFunc: AsyncCompiledExpression, env: nunjucks.Environment, context: object, frame: runtime.Frame, rnt: typeof runtime) {
+		const expressionPromise = compiledFunc(env, context, frame, rnt);
+		this.runningExpressions.push(expressionPromise);
+		return expressionPromise;
+	}
+
+	async waitAllExpressions(): Promise<void> {
+		await Promise.all(this.runningExpressions);
+		this.runningExpressions = []; // Clear the array after all expressions are resolved
 	}
 }
