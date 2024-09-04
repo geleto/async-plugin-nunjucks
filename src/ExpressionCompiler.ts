aa9d705ab23e673b2a9cce6c90d5e62b184e87aa
Angel Popov <geleto@gmail.com>
2024-09-04 12:15:46 +0100

    feat: Expression compiler result function returns a Promise

--- src/ExpressionCompiler.ts
+++ src/ExpressionCompiler.ts
@@ -1,7 +1,7 @@
 import * as nunjucks from 'nunjucks';
 import { compiler, runtime, nodes } from 'nunjucks';
 
-type CompiledExpression = (env: nunjucks.Environment, context: object, frame: runtime.Frame, rnt: typeof runtime) => any;
+type AsyncCompiledExpression = (env: nunjucks.Environment, context: object, frame: runtime.Frame, rnt: typeof runtime) => Promise<any>;
 
 export class ExpressionCompiler extends compiler.Compiler {
 	constructor(name: string, throwOnUndefined: boolean) {
@@ -17,10 +17,15 @@ export class ExpressionCompiler extends compiler.Compiler {
 		this._emit(';');
 
 		const code = this.getCode();
-		return new Function('context', 'env', 'runtime', code + '; return expressionResult;');
+		return new Function('context', 'env', 'runtime',
+			`return (async () => {
+                ${code}
+                return expressionResult;
+            })();`
+		) as AsyncCompiledExpression;
 	}
 
-	evaluate(compiledFunc: CompiledExpression, env: nunjucks.Environment, context: object, frame: runtime.Frame, rnt: typeof runtime) {
+	evaluate(compiledFunc: AsyncCompiledExpression, env: nunjucks.Environment, context: object, frame: runtime.Frame, rnt: typeof runtime) {
 		return compiledFunc(env, context, frame, rnt);
 	}
 }
