955f6c422b44e303abe2648b150b9e602d002cfd
Angel Popov <geleto@gmail.com>
2024-09-09 14:25:28 +0100

    refactor: separate function to patch any methods

--- src/AsyncEnvironment.ts
+++ src/AsyncEnvironment.ts
@@ -5,29 +5,39 @@ import { ASTWalker } from './ASTWalker';
 export class AsyncEnvironment extends nunjucks.Environment {
 	private static asyncExtension = new AsyncExtension();
 	private static astWalker = new ASTWalker();
-	static initializedParseAsRoot = false;
+	static patchedMethods = false;
 
 	constructor(loader?: nunjucks.ILoaderAny | nunjucks.ILoaderAny[], opts?: nunjucks.ConfigureOptions) {
 		super(loader, opts);
 
-		if (!AsyncEnvironment.initializedParseAsRoot) {
-			AsyncEnvironment.initializedParseAsRoot = true;
-			const originalParseAsRoot = nunjucks.parser.Parser.prototype.parseAsRoot;
-			nunjucks.parser.Parser.prototype.parseAsRoot = function (): nunjucks.nodes.Root {
-				const ast = originalParseAsRoot.call(this);
-				//check if the environment has the async extension and only then process the AST
-				const extensions = this.extensions as nunjucks.Extension[];
-				for (const ext of extensions) {
-					if (ext === AsyncEnvironment.asyncExtension) {
-						AsyncEnvironment.astWalker.insertEmptyLineAtStart(ast);
-						return ast;
-					}
-				}
-				return ast;
-			};
+		if (!AsyncEnvironment.patchedMethods) {
+			AsyncEnvironment.patchedMethods = true;
+
+			this.patchMethods();
+
+
 		}
 		this.addExtension('AsyncExtension', AsyncEnvironment.asyncExtension);
 	}
+
+	patchMethods() {
+		//patch parseAsRoot to get the AST for processing
+		const originalParseAsRoot = nunjucks.parser.Parser.prototype.parseAsRoot;
+		nunjucks.parser.Parser.prototype.parseAsRoot = function (): nunjucks.nodes.Root {
+			const ast = originalParseAsRoot.call(this);
+			//check if the environment has the async extension and only then process the AST
+			const extensions = this.extensions as nunjucks.Extension[];
+			for (const ext of extensions) {
+				if (ext === AsyncEnvironment.asyncExtension) {
+					AsyncEnvironment.astWalker.insertEmptyLineAtStart(ast);
+					return ast;
+				}
+			}
+			return ast;
+		};
+	}
+
+
 
 	// Asynchronously render a template from a file with AST modification and caching
 	async renderAsync(templateName: string, context: object): Promise<string> {
