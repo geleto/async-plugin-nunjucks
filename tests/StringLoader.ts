e426af708f989218efec646c4e4aa583a7ac90f6
Angel Popov <geleto@gmail.com>
2024-09-25 13:58:40 +0100

    fix: working StringLoader

--- tests/StringLoader.ts
+++ tests/StringLoader.ts
@@ -1,25 +1,21 @@
 import * as nunjucks from 'nunjucks';
 
 export class StringLoader implements nunjucks.ILoader {
-	private cache: Map<string, string>;
-
-	constructor() {
-		this.cache = new Map<string, string>();
-	}
+	private templates = new Map<string, string>();
 
 	getSource(name: string): nunjucks.LoaderSource {
-		if (!this.cache.has(name)) {
-			throw new Error(`Template ${name} not found in cache`);
+		if (!this.templates.has(name)) {
+			throw new Error(`Template ${name} not found`);
 		}
 
 		return {
-			src: this.cache.get(name)!,
+			src: this.templates.get(name)!,
 			path: name,
 			noCache: false
 		};
 	}
 
 	addTemplate(name: string, content: string): void {
-		this.cache.set(name, content);
+		this.templates.set(name, content);
 	}
 }
